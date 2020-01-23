6e57b5cfed2a72f86e4551aba590785b138ebd8c
Tyler <tylerclark314@gmail.com>
2020-01-22 22:42:15 -0800

    added js functionality for product display

--- script.js
+++ script.js
@@ -1,8 +1,44 @@
 function toggleNav() {
-    if($("nav").css("display") == "flex"){
+    if ($("nav").css("display") == "flex") {
         $("nav").css("display", "none");
     }
     else {
         $("nav").css("display", "flex");
     }
 }
+
+function displayProduct(productType) {
+    $.getJSON(productType, json => {
+        json.forEach(elem => {
+            $(".container").append(`
+        <div class="product-container">
+            <img src="${elem.imgSrc}" alt="${elem.alt}" class="product-img">
+            <div class="description-pricing-container">
+                <p class="product-description">${elem.description}</p>
+                <div class="pricing-container">
+                    <p class="product-pricing pricing-text">${elem.price}</p>
+                    <p>Pricing includes paypal fees and shipping</p>
+                </div>
+            </div>
+        </div>
+    `)
+        })
+    })
+}
+
+function getProducts() {
+    if(window.location.href.indexOf("drums") != -1){
+        return "drums.json";
+    }
+    else if(window.location.href.indexOf("rattles") != -1){
+        return "rattles.json";
+    }
+    else if(window.location.href.indexOf("glasswork" != -1)){
+        return "glasswork.json";
+    }
+    else {
+        return "";
+    }
+}
+
+{displayProduct(getProducts())}
